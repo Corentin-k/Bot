cbe5ad9b02bc1ca5f373e8c4deb0384176944c8c
Kevin <kjabiol22@gmail.com>
2023-09-23 12:17:08 +0200

    ajout du status

--- src/events/ready.ts
+++ src/events/ready.ts
@@ -1,10 +1,16 @@
+const {Client, Intents} = require("discord.js")
+
 module.exports = {
     name: "ready", 
     once: true, 
     async execute (client: any) {
         console.log("Bot launched !");
-
+        
         const devGuild = client.guilds.cache.get(process.env.SERV_ID as string);
         devGuild?.commands.set(client.commands.map((cmd: any) => cmd))
+
+        client.user.setActivity({
+            name: 'travailler'
+        })
     }
 }
