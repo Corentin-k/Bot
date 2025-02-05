44b4812708e0949b833ff18de589f63f0b90be86
Corentin-k <c.kervagoret@outlook.fr>
2025-02-05 19:09:52 +0100

    correction db

--- backend/db.sql
+++ backend/db.sql
@@ -1,6 +1,7 @@
 CREATE DATABASE IF NOT EXISTS owlbot;
 USE owlbot;
 
+-- Table pour stocker les plannings utilisateurs
 CREATE TABLE IF NOT EXISTS user_plannings (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id VARCHAR(255) NOT NULL UNIQUE,
@@ -8,13 +9,17 @@ CREATE TABLE IF NOT EXISTS user_plannings (
     user_name TEXT NOT NULL
 ) ENGINE=InnoDB;
 
-
+-- Table pour stocker les événements planifiés
 CREATE TABLE IF NOT EXISTS events (
     id INT AUTO_INCREMENT PRIMARY KEY,
     user_id VARCHAR(255) NOT NULL,
+    title VARCHAR(255) NOT NULL,
+    description TEXT NOT NULL,
     dateEvent DATE NOT NULL,
     heureEvent TIME NOT NULL,
-    textEvent VARCHAR(255) NOT NULL,
+    location VARCHAR(255) NOT NULL,
+    everyoneTF BOOLEAN NOT NULL,
+    numberCallback INT NOT NULL,
     notified BOOLEAN DEFAULT 0,
-    UNIQUE (user_id, textEvent) 
+    UNIQUE (user_id, title, dateEvent, heureEvent)
 ) ENGINE=InnoDB;
